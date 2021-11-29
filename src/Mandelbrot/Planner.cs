76dee39f32fc1b3c3134585ea955a30cea94e46c
Frederic Vogels <frederic.vogels@ucll.be>
2021-11-29 14:29:45 +0100

    code

---
+++ src/Mandelbrot/Planner.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandelbrot
+{
+    public interface IPlanner
+    {
+        /// <summary>
+        /// Number of jobs.
+        /// </summary>
+        public int JobCount { get; }
+
+        /// <summary>
+        /// Returns the index-th job.
+        /// </summary>
+        /// <param name="index">Index of job to return.</param>
+        /// <returns>The index-th job</returns>
+        public Action Job( int index );
+    }
+
+    public class PixelPlanner : IPlanner
+    {
+        private readonly IList<Mandelbrot> mandelbrots;
+
+        public PixelPlanner( IList<Mandelbrot> mandelbrots )
+        {
+            this.mandelbrots = mandelbrots;
+        }
+
+        public int JobCount => mandelbrots[0].Height * mandelbrots[0].Width * mandelbrots.Count;
+
+        public Action Job( int index )
+        {
+            var width = mandelbrots[0].Width;
+            var height = mandelbrots[0].Height;
+            var pixelCount = width * height;
+            var mandelbrotIndex = index / pixelCount;
+            var pixelIndex = index / pixelCount;
+            var x = pixelIndex % width;
+            var y = pixelIndex / width;
+
+            return () => this.mandelbrots[mandelbrotIndex].ComputeSingle( x, y );
+        }
+    }
+
+    public class RowPlanner : IPlanner
+    {
+        private readonly IList<Mandelbrot> mandelbrots;
+
+        public RowPlanner( IList<Mandelbrot> mandelbrots )
+        {
+            this.mandelbrots = mandelbrots;
+        }
+
+        public int JobCount => mandelbrots[0].Height * mandelbrots.Count;
+
+        public Action Job( int index )
+        {
+            var rowCount = mandelbrots[0].Height;
+            var mandelbrotIndex = index / rowCount;
+            var rowIndex = index % rowCount;
+
+            return () => this.mandelbrots[mandelbrotIndex].ComputeRow( rowIndex );
+        }
+    }
+
+    public class FramePlanner : IPlanner
+    {
+        private readonly IList<Mandelbrot> mandelbrots;
+
+        public FramePlanner( IList<Mandelbrot> mandelbrots )
+        {
+            this.mandelbrots = mandelbrots;
+        }
+
+        public int JobCount => mandelbrots.Count;
+
+        public Action Job( int index )
+        {
+            return () => this.mandelbrots[index].ComputeAll();
+        }
+    }
+
+    public class MonolithPlanner : IPlanner
+    {
+        private readonly IList<Mandelbrot> mandelbrots;
+
+        public MonolithPlanner( IList<Mandelbrot> mandelbrots )
+        {
+            this.mandelbrots = mandelbrots;
+        }
+
+        public int JobCount => 1;
+
+        public Action Job( int index )
+        {
+            return () => {
+                foreach ( var mandelbrot in this.mandelbrots )
+                {
+                    mandelbrot.ComputeAll();
+                }
+            };
+        }
+    }
+}
---
+++ src/Mandelbrot/Schedulers.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mandelbrot
+{
+    public interface IScheduler
+    {
+        /// <summary>
+        /// Run all jobs.
+        /// </summary>
+        /// <param name="planner">Planner that provides all jobs.</param>
+        void Schedule( IPlanner planner );
+    }
+
+    public class SingleThreadScheduler : IScheduler
+    {
+        public void Schedule( IPlanner planner )
+        {
+            for ( var i = 0; i != planner.JobCount; ++i )
+            {
+                planner.Job( i )();
+            }
+        }
+    }
+
+    public class ManualThreadingScheduler : IScheduler
+    {
+        public void Schedule( IPlanner planner )
+        {
+            var nthreads = Environment.ProcessorCount;
+            var threads = new List<Thread>();
+            var row = 0;
+
+            for ( var i = 0; i != nthreads; ++i )
+            {
+                var thread = new Thread( () =>
+                {
+                    while ( true )
+                    {
+                        var jobIndex = Interlocked.Increment( ref row );
+
+                        if ( jobIndex >= planner.JobCount )
+                        {
+                            break;
+                        }
+
+                        planner.Job( jobIndex )();
+                    }
+                } );
+
+                thread.Start();
+                threads.Add( thread );
+            }
+
+            foreach ( var thread in threads )
+            {
+                thread.Join();
+            }
+        }
+    }
+
+    public class ParallelScheduler : IScheduler
+    {
+        public void Schedule( IPlanner planner )
+        {
+            var result = Parallel.For( 0, planner.JobCount, jobIndex =>
+            {
+                planner.Job( jobIndex )();
+            } );
+        }
+    }
+
+    public class EnumerableScheduler : IScheduler
+    {
+        public void Schedule( IPlanner planner )
+        {
+            Enumerable.Range( 0, planner.JobCount ).AsParallel().ForAll( jobIndex =>
+            {
+                planner.Job( jobIndex )();
+            } );
+        }
+    }
+
+    public class TaskScheduler : IScheduler
+    {
+        public void Schedule( IPlanner planner )
+        {
+            var tasks = Enumerable.Range( 0, planner.JobCount ).Select( jobIndex =>
+            {
+                return new Task( () =>
+                {
+                    planner.Job( jobIndex )();
+                } );
+            } );
+
+            Task.WhenAll( tasks ).Wait();
+        }
+    }
+
+    public class ThreadproolScheduler : IScheduler
+    {
+        public void Schedule( IPlanner planner )
+        {
+            var jobsDone = 0;
+            var lck = new object();
+
+            for ( var jobIndex = 0; jobIndex != planner.JobCount; ++jobIndex )
+            {
+                var i = jobIndex;
+
+                ThreadPool.QueueUserWorkItem( _ =>
+                {
+                    planner.Job( i )();
+
+                    Interlocked.Increment( ref jobsDone );
+
+                    lock ( lck )
+                    {
+                        Monitor.Pulse( lck );
+                    }
+                } );
+            }
+
+            while ( jobsDone < planner.JobCount )
+            {
+                lock ( lck )
+                {
+                    Monitor.Wait( lck );
+                }
+            }
+        }
+    }
+}
